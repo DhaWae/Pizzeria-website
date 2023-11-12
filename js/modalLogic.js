909917388b16ea0a9aac3c1b2687a076cf198659
Jesper <Jesperrudegran@gmail.com>
2023-11-12 19:55:16 +0100

    modal code cleanups

--- js/modalLogic.js
+++ js/modalLogic.js
@@ -2,39 +2,49 @@ const modal = document.querySelector('[data-modal]');
 const openRegistrationButton = document.querySelector('[data-open-register-modal]');
 const registrationModal = document.querySelector('[data-register-modal]');
 const infoBox = document.querySelector('[data-info-box]');
-const loginBackground = document.querySelector('[data-login-background]');
-
+const modalBlurBackground = document.querySelector('[data-login-background]');
 
 var regEmailInput = document.getElementById('val1dest');
-var regEmailValue;
 var regFirstNameInput = document.getElementById('first-name');
-var regFirstNameValue;
 var regLastNameInput = document.getElementById('last-name');
-var regLastNameValue;
 var regPhoneNumberInput = document.getElementById('phone-number');
-var regPhoneNumberValue;
 var regPasswordInput = document.getElementById('password1');
+
+var regEmailValue;
+var regFirstNameValue;
+var regLastNameValue;
+var regPhoneNumberValue;
 var regPasswordValue;
 
-var isInfoOpen = false;
-var isRegistrationOpen = false;
+function openLoginModal() {
+  modal.style.display = 'flex';
+  modalBlurBackground.style.display = 'block';
+  setInfoBoxPosition();
+  document.cookie ="modalOpen = login";
+}
 
 function closeLoginModal() {
   modal.style.display = 'none';
-  loginBackground.style.display = 'none';
+  modalBlurBackground.style.display = 'none';
   infoBox.style.display = 'none';
-  isInfoOpen = false;
-  storeElementState();
+  document.cookie ="modalOpen = none";
+}
+
+function openRegistrationModal() {
+  registrationModal.style.display = 'flex';
+  modalBlurBackground.style.display = 'block';
+  setInfoBoxPosition();
+  document.cookie ="modalOpen = register";
+  transferLoginValues();
 }
 
 function closeRegistrationModal() {
   registrationModal.style.display = 'none';
-  loginBackground.style.display = 'none';
+  modalBlurBackground.style.display = 'none';
   infoBox.style.display = 'none';
-  isRegistrationOpen = false;
-  storeElementState();
+  clearRegistrationInputValues();
+  document.cookie ="modalOpen = none";
 }
-
 
 function handleSuccessfulLogin() {
   let queryString = window.location.search;
@@ -50,48 +60,23 @@ function handleSuccessfulRegistration() {
   let urlParams = new URLSearchParams(queryString);
   let regStat = urlParams.get('register');
   if (regStat === 'success') {
-    registrationModal.style.display = 'none';
-    // Clear stored registration data when modal closes
+    clearRegistrationInputValues();
+    storeRegisterValues();
+    closeRegistrationModal();
   }
 }
 
-// Add an event listener to open the modal
-function openLoginModal() {
-  modal.style.display = 'flex';
-  loginBackground.style.display = 'block';
-  isInfoOpen = true;
-  setInfoBoxPosition();
-  storeElementState();
-}
-
 // Function to store the state of the elements in local storage
-function storeElementState() {
-  localStorage.setItem('modalDisplay', modal.style.display);
-  localStorage.setItem('loginBackgroundDisplay', loginBackground.style.display);
-  localStorage.setItem('infoStatus', isInfoOpen);
-  localStorage.setItem('registrationModalDisplay', isRegistrationOpen);
-  console.log("SETTING REGEMAILVALUE TO:" + regEmailValue);
+function storeRegisterValues() {
   localStorage.setItem('regEmailValue', regEmailValue) || "";
-  console.log("REGEMAILVALUE IS" + localStorage.getItem('regEmailValue'));
   localStorage.setItem('regFirstNameValue', regFirstNameValue) || "";
   localStorage.setItem('regLastNameValue', regLastNameValue) || "";
   localStorage.setItem('regPhoneNumberValue', regPhoneNumberValue) || "";
   localStorage.setItem('regPasswordValue', regPasswordValue) || "";
 }
 
-// Function to restore the state of the elements from local storage
-function restoreElementState() {
-  console.log(localStorage.getItem('modalDisplay') + "moddisplay");
-  if (localStorage.getItem('modalDisplay') == 'flex') {
-    modal.style.display = 'flex';
-  } else { modal.style.display = 'none'; }
-  console.log(localStorage.getItem('registrationModalDisplay') + "regdisplay");
-  if (localStorage.getItem('registrationModalDisplay') == 'true') {
-    registrationModal.style.display = 'flex';
-  } else { registrationModal.style.display = 'none'; }
-
+function restoreRegisterValues() {
   try {
-    console.log("SETTING REGEMAILINPUT TO:" + localStorage.getItem('regEmailValue'));
     regEmailInput.value = localStorage.getItem('regEmailValue');
     regFirstNameInput.value = localStorage.getItem('regFirstNameValue');
     regLastNameInput.value = localStorage.getItem('regLastNameValue');
@@ -100,39 +85,30 @@ function restoreElementState() {
   } catch (error) {
     console.log("No registration modal values to restore");
   }
-  
-  
-  //registrationModal.style.display = localStorage.getItem('registrationModalDisplay') || 'none';
-  console.log(localStorage.getItem('registrationModalDisplay') + "regdisplay");
-  loginBackground.style.display = localStorage.getItem('loginBackgroundDisplay') || 'none';
-
-  isInfoOpen = localStorage.getItem('infoStatus') === 'true';
-  setInfoBoxPosition();
 }
 
-// Restore the element state when the page loads
-restoreElementState();
+// Function to restore the state of the elements from local storage
+function restoreElementState() {
+  if (document.cookie.includes("modalOpen=login")) {
+    openLoginModal();
+  }
+  if (document.cookie.includes("modalOpen=register")) {
+    openRegistrationModal();
+  }
+  restoreRegisterValues();
+}
 
 // Add an event listener to open the registration modal
 openRegistrationButton.addEventListener('click', () => {
-  
   infoBox.style.display = 'none';
-
-  // Get email and password input values
-  const emailInput = document.getElementById('val1');
-  const emailValue = emailInput.value;
-  const passwordInput = document.getElementById('password');
-  const passwordValue = passwordInput.value;
-
-  modal.style.display = 'none';
-  registrationModal.style.display = 'flex';
-
-  // Set the input values in the registration modal
-  const registrationEmailInput = document.getElementById('val1dest');
-  const registrationPasswordInput = document.getElementById('password1');
-  registrationEmailInput.value = emailValue;
-  registrationPasswordInput.value = passwordValue;
+  closeLoginModal();
+  openRegistrationModal();
 });
+
+function transferLoginValues() {
+  document.getElementById("val1dest").value = document.getElementById("val1").value
+  document.getElementById("password1").value = document.getElementById("password").value
+}
 
 function setInfoBoxPosition() {
   // some formula to get the inner width of the viewport compared to just documeny.body.clientWidth which isnt accurate
@@ -159,7 +135,7 @@ window.addEventListener('resize', setInfoBoxPosition);
 document.addEventListener('keydown', (e) => {
   if (e.key === 'Escape') {
     closeLoginModal();
-    registrationModal.style.display = 'none';
+    closeRegistrationModal();
   }
 });
 
@@ -170,7 +146,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 // Function to check for errors in the URL query parameters
-function CheckForErrors() {
+function checkForErrors() {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const error = urlParams.get('error');
@@ -185,25 +161,9 @@ function CheckForErrors() {
   }
 }
 
-// Call the CheckForErrors function to check for errors on page load
-CheckForErrors();
-
 addEventListener("beforeunload", function() {
-  if(modal.style.display == "" || modal.style.display == "none") {
-    console.log("login modal closed");
-    isInfoOpen = false;
-  } else {
-    isInfoOpen = true;
-  }
-
-  if(registrationModal.style.display == "" || registrationModal.style.display == "none") {
-    console.log("reg modal closed");
-    isRegistrationOpen = false;
-  } else {
-    isRegistrationOpen = true;
-  }
   setRegistrationInputValues();
-  storeElementState();
+  storeRegisterValues();
 });
 
 function setRegistrationInputValues() {
@@ -217,6 +177,15 @@ function setRegistrationInputValues() {
   regPhoneNumberValue = regPhoneNumberInput.value;
   regPasswordInput = document.getElementById('password1');
   regPasswordValue = regPasswordInput.value;
+}
+
+function clearRegistrationInputValues() {
+  regEmailInput.value = "";
+  regFirstNameInput.value = "";
+  regLastNameInput.value = "";
+  regPhoneNumberInput.value = "";
+  regPasswordInput.value = "";
+  storeRegisterValues();
 }
 
 function togglePasswordReg() {
@@ -259,3 +228,11 @@ function openModalCookie() {
   document.cookie = "modalStatus=open";
   openLoginModal();
 }
+
+function switchModal() {
+  modal.style.display = 'none';
+  registrationModal.style.display = 'flex';
+}
+
+restoreElementState();
+checkForErrors();
