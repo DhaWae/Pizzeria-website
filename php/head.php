27eb6f4365f0e6dd8741ef6cbdf314bcfadb0aec
Jesper <Jesperrudegran@gmail.com>
2023-11-22 09:35:27 +0100

    theme color meta-tag

--- php/head.php
+++ php/head.php
@@ -8,6 +8,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta name="color-scheme" content="srgb">
   <link rel="profile" href="../sRGB2014.icc">
+  <meta name="theme-color" content="#9b491d">
   <link rel="stylesheet" href="../css/styles.css" />
   <link rel="stylesheet" href="../css/header.css" />
   <link rel="stylesheet" href="../css/modal.css" />
